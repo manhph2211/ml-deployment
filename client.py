cf1329b4e5050c9a6ac1c3a48e07d57e447dacda
manhph2211 <maxph22112000@gmail.com>
2023-05-25 17:05:57 +0700

    .

---
+++ client.py
@@ -0,0 +1,8 @@
+import requests
+
+files = {'data': open('sample_text.txt','rb')}
+response = requests.post('http://localhost:8080/predictions/waveglow_synthesizer', files=files)
+data = response.content
+
+with open('audio.wav', 'wb') as audio_file:
+    audio_file.write(data)
